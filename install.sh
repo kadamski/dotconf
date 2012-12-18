e69a14fed1dcc75a77f56a122adc7fe2bc77488e
Krzysztof A. Adamski <k@japko.eu>
2012-12-18 11:43:14 +0100

    install.sh: Use test instead of [[.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 DOTFILES='screenrc vimrc vim'
 
 for F in ${DOTFILES}; do
-    if [[ ! -f ${HOME}/.${F} ]]; then
+    if [ ! -f ${HOME}/.${F} ]; then
         ln -s ${PWD}/${F} ${HOME}/.${F}
     fi
 done

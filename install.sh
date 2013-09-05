1384c6cb82dcfb5519e8b86c26116c24aa638149
Krzysztof Adamski <k@japko.eu>
2013-09-05 20:01:52 +0200

    i3 config

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 
-DOTFILES='screenrc vimrc vim tmux.conf bashrc'
+DOTFILES='screenrc vimrc vim tmux.conf bashrc i3status.conf i3/config'
 
 for F in ${DOTFILES}; do
     if [ ! -e ${HOME}/.${F} ]; then
+        mkdir -p `dirname ${HOME}/.${F}`
         ln -s ${PWD}/${F} ${HOME}/.${F}
     fi
 done

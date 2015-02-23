7b1ba0985875c748018439fe76e28e6d4c73d113
Krzysztof Adamski <k@japko.eu>
2015-02-23 19:28:55 +0100

    compilevim: some fixes

--- compilevim.sh
+++ compilevim.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+# apt-get install mercurial
+mkdir -p ~/BUILD/
 cd ~/BUILD/
 hg clone https://code.google.com/p/vim/
 cd vim
 ./configure --with-features=huge --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7-config --enable-perlinterp --enable-luainterp --enable-gui=gnome2 --enable-cscope --prefix=${HOME}/vim/ 
 make VIMRUNTIMEDIR=${HOME}/vim/runtime/
 make install
+cp -r ~/BUILD/vim/runtime/ ${HOME}/vim/

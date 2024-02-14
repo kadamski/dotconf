c527087e990a1cceaee74e2f948680995f280587
Krzysztof Adamski <k@japko.eu>
2024-02-14 23:46:28 +0100

    nvim: complete: add comple for cmdline

--- nvim/lua/plugins/complete.lua
+++ nvim/lua/plugins/complete.lua
@@ -65,5 +65,24 @@ return {
                 end
             },
         })
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
+        })
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
     end,
 }

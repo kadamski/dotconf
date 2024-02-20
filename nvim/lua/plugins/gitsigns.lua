d0f0f8ddbb9a13e1bf33801863745d2d0dd3a933
Krzysztof Adamski <k@japko.eu>
2024-02-20 14:04:02 +0100

    nvim: gitsigns: add some keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -10,10 +10,21 @@ return {
                     name = "gitsigns",
                     b = { function() gs.blame_line({full=true}) end, "blame_line" },
                     t = { gs.toggle_current_line_blame, "blame line toggle" },
-                    s = { gs.diffthis, "diffthis" },
+                    d = { gs.diffthis, "diffthis" },
                     p = { gs.preview_hunk, "preview hunk" },
+                    s = { gs.stage_hunk, "stage hunk" },
+                    S = { gs.undo_stage_hunk, "undo last stage" },
+                    f = { gs.stage_buffer, "stage file" },
                 }
             }, { prefix = "<leader>", buffer=bufnr })
+
+            wk.register({
+                g = { gs.next_hunk, "Next Git hunk" },
+            }, { prefix = "]", buffer=bufnr })
+
+            wk.register({
+                g = { gs.prev_hunk, "Prev Git hunk" },
+            }, { prefix = "[", buffer=bufnr })
         end
     },
     config = function(_, opts)

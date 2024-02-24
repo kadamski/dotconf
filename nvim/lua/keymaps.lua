d9aaf60b9cf5919ba1f7a63a0e93c48b4626ef9c
Krzysztof Adamski <k@japko.eu>
2024-02-25 00:49:43 +0100

    nvim: keymaps: more leader keys

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -21,6 +21,8 @@ wk.register({
         b = { telescope.buffers, "buffers" },
         h = { telescope.help_tags, "help tags" },
         g = { telescope.live_grep, "grep" },
+        m = { telescope.marks, "marks" },
+        s = { telescope.lsp_document_symbols, "document symbols" },
     }
 }, { prefix = "<leader>" })
 
@@ -33,6 +35,7 @@ wk.register({
         r = { vim.lsp.buf.references, "references" },
         c = { vim.lsp.buf.code_action, "code action" },
         h = { vim.lsp.buf.hover, "hover" },
+        n = { vim.lsp.buf.rename, "rename" },
     }
 }, { prefix = "<leader>" })
 
@@ -58,7 +61,7 @@ local function toggle_telescope(harpoon_files)
     }):find()
 end
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-vim.keymap.set("n", "<C-\\>", function() harpoon:list():prev({ui_nav_wrap = true}) end)
+-- vim.keymap.set("n", "<C-\\>", function() harpoon:list():prev({ui_nav_wrap = true}) end)
 
 wk.register({
     h = {
@@ -69,6 +72,45 @@ wk.register({
         p = { function() harpoon:list():prev({ui_nav_wrap = true}) end, "previous mark" },
     }
 }, { prefix = "<leader>" })
+
+-- bufferline
+wk.register({
+    b = {
+        name = "bufferline",
+        p = { function() vim.cmd(":BufferLinePick") end, "pick" },
+        l = { function() vim.cmd(":BufferLineCycleNext") end, "next" },
+        h = { function() vim.cmd(":BufferLineCyclePrev") end, "prev" },
+        m = { function() vim.cmd(":BufferLineMoveNext") end, "move next" },
+        n = { function() vim.cmd(":BufferLineMovePrev") end, "move prev" },
+    }
+}, { prefix = "<leader>" })
+vim.keymap.set("n", "<M-1>", ":BufferLineGoToBuffer 1 <CR>", {})
+vim.keymap.set("n", "<M-2>", ":BufferLineGoToBuffer 2 <CR>", {})
+vim.keymap.set("n", "<M-3>", ":BufferLineGoToBuffer 3 <CR>", {})
+vim.keymap.set("n", "<M-4>", ":BufferLineGoToBuffer 4 <CR>", {})
+vim.keymap.set("n", "<M-5>", ":BufferLineGoToBuffer 5 <CR>", {})
+vim.keymap.set("n", "<M-h>", ":BufferLineCyclePrev<CR>", {})
+vim.keymap.set("n", "<M-l>", ":BufferLineCycleNext<CR>", {})
+
+-- toggleterm
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
+
+-- noice
+wk.register({
+    n = {
+        name = "noice",
+        d = { function() vim.cmd(":Noice dismiss") end, "dismiss noice"},
+        t = { function() vim.cmd(":Noice telescope") end, "telescope"},
+    }
+}, { prefix = "<leader>" })
+
+-- alpha
+wk.register({
+    a = {
+        function() vim.cmd(":Alpha") end,
+        "alpha"
+    }
+}, { prefix = "<leader>"})
 
 -- auto-session
 wk.register({

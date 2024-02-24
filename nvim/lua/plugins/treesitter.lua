edd87c4e7bd3817edcd0d99ab87126b7141018f8
Krzysztof Adamski <k@japko.eu>
2024-02-25 00:50:03 +0100

    nvim: treesitter: add text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,30 @@ return {
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
+        textobjects = {
+            select = {
+                enable = true,
+                keymaps = {
+                    ["af"] = { query ="@function.outer", desc = "a function TO" },
+                    ["if"] = { query ="@function.inner", desc = "inner function TO" },
+                }
+            },
+            move = {
+                enable = true,
+                goto_next_start = {
+                    ["]f"] = { query ="@function.outer", desc = "Next function start" },
+                },
+                goto_next_end = {
+                    ["]F"] = { query ="@function.outer", desc = "Next function end" },
+                },
+                goto_previous_start = {
+                    ["[f"] = { query ="@function.outer", desc = "Nrev function start" },
+                },
+                goto_previous_end = {
+                    ["[F"] = { query ="@function.outer", desc = "Nrev function end" },
+                }
+            },
+        },
     },
     config = function (_, opts)
         local configs = require("nvim-treesitter.configs")

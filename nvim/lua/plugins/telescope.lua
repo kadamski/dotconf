a637e6cc30a800b034f663191a6205662bf60e24
Krzysztof Adamski <k@japko.eu>
2025-08-24 16:11:48 +0200

    nvim: telescope: ripgrep required for live_grep

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
     "nvim-telescope/telescope.nvim", tag = '0.1.5',
     event = "VeryLazy",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
 }
